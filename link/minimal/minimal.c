888231d7d530f19407e36089b04aa19ba16bea85
pplonepiece <pplonepiece@126.com>
2018-04-26 21:40:55 +0800

    fix %% in un-inline asm code

--- link/minimal/minimal.c
+++ link/minimal/minimal.c
@@ -1,5 +1,6 @@
 char *str = "Hello Wolrd\n";
 
+// 双%符号是用来扩展内联汇编代码，后面会跟input, output等
 void print() {
     asm("movl $13, %%edx \n\t"
             "movl %0, %%ecx \n\t"
@@ -9,9 +10,11 @@ void print() {
             ::"r"(str):"edx","ecx","ebx");
 }
 
+// 如果不适用内联汇编的写法，直接使用单%符号即可
+// 否则会报 bad register name '%%ecx' 错误
 void exit() {
-    asm("movl $42, %%ebx \n\t"
-            "movl $1, %%eax \n\t"
+    asm("movl $42, %ebx \n\t"
+            "movl $1, %eax \n\t"
             "int $0x80 \n\t");
 }
 
